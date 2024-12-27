93db04cb9f7f9ccd13d823062f8f7f3d8e66ef0e
Hassan Said <92964808+hassan010802002@users.noreply.github.com>
2024-12-27 23:41:16 +0200

    new update

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: bash setup.sh && pip install -r requirements.txt && appium -p 5000 && uvicorn appium_server:app --host 0.0.0.0 --port 8000
+web: bash setup.sh && pip install -r requirements.txt && appium -a 0.0.0.0 -p 5000 
+web: uvicorn appium_server:app --host 0.0.0.0 --port 8000
--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 # Install Node.js and Appium globally
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
-npm install -g appium
+npm install -g appium@2.12.1
+appium driver install uiautomator2@3.8.1
+appium driver install flutter@2.9.2
+appium driver install chromium@1.4.4
