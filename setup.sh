90735f79f9b95e2f9bd96054593a2429ffa63445
Hassan Said <92964808+hassan010802002@users.noreply.github.com>
2024-12-28 01:07:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Install Node.js and Appium globally
 echo "Running setup.sh..."
-npm install -g appium@2.12.1
+
+# Install dependencies (remove npm update as it's redundant with npm install)
+npm install --omit=dev
+
+# Install Appium globally if it's not already installed
+npm list -g appium || npm install -g appium@2.12.1
+
+# Install necessary Appium drivers
 appium driver install uiautomator2@3.8.1
 appium driver install flutter@2.9.2
 appium driver install chromium@1.4.4
+
 echo "Appium setup completed."
